b72b9e210f205e1f9e8c8f98ae7e579b3815f313
= <=>
2021-10-22 16:44:20 +0200

    added service-example

--- service-example.js
+++ service-example.js
@@ -9,13 +9,16 @@ const host = 'amqp://localhost';
 /** 
  * subscribe to river(s), and do some work if it gets an event.
  */
-subscribe.subscribe(host, "info", (msg, channel) => {
-    console.log(" [x] Received: %s", msg.content.toString());
-
-    //Send an event back into the rapid 
-    channel.publish(subscribe.rapid, "error", Buffer.from('info message sent'));
-});
-
 subscribe.subscribe(host, "display", (msg, channel) => {
     console.log(" [x] Received: %s", msg.content.toString());
 });
+
+/** 
+ *  More subscriptions can be added by calling the subscribe function again:
+ */
+// subscribe.subscribe(host, "info", (msg, channel) => {
+//     console.log(" [x] Received: %s", msg.content.toString());
+
+//     //Send an event back into the rapid 
+//     channel.publish(subscribe.rapid, "error", Buffer.from('info message sent'));
+// });
